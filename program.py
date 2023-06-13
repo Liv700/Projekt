5383f74622e67c3a98fc228372a68ebf01a9d8a0
Liwia Sikorska <liwia.sikorska@gmail.com>
2023-06-13 21:20:30 +0200

    Dodano wczytanie z pliku .json

--- program.py
+++ program.py
@@ -13,6 +13,19 @@ def parsuj_argumenty():
     file2 = sys.argv[2]
     return file1, file2
 
+def wczytaj_json(file):
+    try:
+        with open(file, 'r') as f:
+            data = json.load(f)
+        return data
+    except FileNotFoundError:
+        print(f"Plik '{file}' nie istnieje.")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print(f"Błąd podczas parsowania pliku '{file}'. Sprawdź poprawność składni JSON.")
+        sys.exit(1)
 
 
 file1, file2 = parsuj_argumenty()
+
+data1 = wczytaj_json(file1)
